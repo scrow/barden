9318753b0ad0b1fbc7227a86f08e476b01bf8bf5
rbarden <reidbarden@gmail.com>
2016-01-19 18:08:54 -0500

    revamp toggle page

--- toggle.php
+++ toggle.php
@@ -1,21 +1,46 @@
+<?php 
+	
+	if(isset($_POST['pyes'])){
+		touch('pants.flg');
+		$status = 'Pants Yes';
+	}
+	
+	if(isset($_POST['pno'])){
+		unlink('pants.flg');
+		$status = 'Pants No';
+	}
+	
+	if(isset($_POST['syes'])){
+		touch('shoes.flg');
+		$status = 'Shoes Yes';
+	}	
+	
+	if(isset($_POST['sno'])){
+		unlink('pants.flg');
+		$status = 'Shoes No';
+	}		
+?>
+
 <html>
 	<head>
-		<title>Toggle ReidBarden.com Status</title>
+		<title>Toggle Reid Barden Pant/Shoe Status</title>
 	</head>
 	<body>
-		<?php
 		
-		if(file_exists('pants.flg')) {
-			unlink('pants.flg');
-			echo('<p>Status set to SHORTS</p>');
-		} else {
-			touch('pants.flg');
-			echo('<p>Status set to PANTS</p>');
-		};
-		
+		<?php 
+			
+			if(isset($status)){
+				echo '<p> $status </p>';
+			}
+			
 		?>
+
+		<form method="post">
+			<label> Pants <button type="submit" name="pyes"> Yes </button> <button type="submit" name="pno"> No </button> </label>
+			<label> Shoes <button type="submit" name="syes"> Yes </button> <button type="submit" name="sno"> No </button> </label>
+		</form>
 		
 		<p><a href="index.php">Go to the homepage</a> or <a href="toggle.php">toggle again</a>.</p>
-		
+
 	</body>
 </html>
